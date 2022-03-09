2fa7809da3ba9d522a3fa650fbfcb96ace984273
Aditya Oli <adityaoligehu@gmail.com>
2022-03-09 21:39:10 +1300

    commit

--- starter/infra/_config.tf
+++ starter/infra/_config.tf
@@ -1,14 +1,16 @@
 terraform {
   backend "s3" {
-    bucket = "udacity-tf-<first_name>" # Update here with your S3 bucket
+    bucket = "udacity-tf-adityaoli-lesson3" # Update here with your S3 bucket
     key    = "terraform/terraform.tfstate"
     region = "us-east-2"
+    encrypt = true
+    profile = "admin"
   }
 }
 
 provider "aws" {
   region = "us-east-2"
-
+  profile = "admin"
   default_tags {
     tags = local.tags
   }
